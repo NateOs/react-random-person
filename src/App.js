205db4ec53b15e1c5786f8a1563642ad80052dc9
nateos <sodjanathan@gmail.com>
2021-03-16 23:37:05 +0000

    Dynamically setting field values from object

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ function App() {
       street: `${number}, ${name}`,
       name: `${first} ${last}`
     }
+    
     setPerson(newPerson)
     setLoading(false)
     setTitle('name')
@@ -49,7 +50,12 @@ function App() {
   }, [])
 
   const handleValue = (e) => {
-    console.log(e.target)
+    if (e.target.classList.contains('icon')) {
+      const newValue = e.target.dataset.label
+      setTitle(newValue)
+      setValue(person[newValue])
+    }
+    
   }
 
   return (
